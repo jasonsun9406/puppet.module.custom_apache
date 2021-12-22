66740b37bd1a9504e66a3e747075c9b7727ffdbb
jsun9406 <jasons@iqmetrix.com>
2021-12-22 11:40:54 -0800

    ad

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class customapache {
 }
 
 class customapache::install {
-  if $facts['windows_edition_custom'] == 'testvalue' {
+  if $facts['windows_edition_custom'] != 'testvalue' {
     exec { 'Check-Service':
       command   => 'write-host work',
       provider  => powershell,
