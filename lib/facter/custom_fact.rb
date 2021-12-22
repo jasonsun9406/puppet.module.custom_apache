e0fb1e374d1e1f2782fc8f40505abae17ea36892
jsun9406 <jasons@iqmetrix.com>
2021-12-22 12:18:26 -0800

    add

--- lib/facter/custom_fact.rb
+++ lib/facter/custom_fact.rb
@@ -1,7 +1,6 @@
 Facter.add('check_nr_exist') do
   confine :osfamily => :windows
   setcode do
-    $serviceExist = Get-Service | where {$_.Name -eq "newrelic-infra"};
     Facter::Core::Execution.execute('
     if (Get-Service | where {$_.Name -eq newrelic-infra})
     {Write-Output check_nr_exist=true}else{Write-Output check_nr_exist=false};
