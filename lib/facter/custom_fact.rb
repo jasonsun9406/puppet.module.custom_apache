a93da954db7a1469dbfddbef28506fcfc068d7d4
jsun9406 <jasons@iqmetrix.com>
2021-12-22 13:02:56 -0800

    add

--- lib/facter/custom_fact.rb
+++ lib/facter/custom_fact.rb
@@ -1,6 +1,6 @@
 Facter.add('check_nr_svc') do
   confine :osfamily => :windows
   setcode do
-    Facter::Core::Execution.execute(%q[powershell "if (get-service -Name newrelic-infra -ErrorAction SilentlyContinue){write-host \"true\"}else{write-host \"false\"}"])
+    Facter::Core::Execution.execute(%q[powershell "if (get-service -Name puppet -ErrorAction SilentlyContinue){write-host \"true\"}else{write-host \"false\"}"])
   end
 end
