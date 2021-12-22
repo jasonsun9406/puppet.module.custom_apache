fd7012e9c147ac956d6864b28204d6c9799d9599
jsun9406 <jasons@iqmetrix.com>
2021-12-22 12:35:38 -0800

    add

--- lib/facter/custom_fact.rb
+++ lib/facter/custom_fact.rb
@@ -1,6 +1,6 @@
 Facter.add('check_nr_exist') do
   confine :osfamily => :windows
   setcode do
-    Facter::Core::Execution.execute(%q[powershell "write-host \"check_nr_exist=true\""])
+    Facter::Core::Execution.execute(%q[powershell "if (get-service){write-host \"true\"}else{write-host \"false\"}"])
   end
 end
