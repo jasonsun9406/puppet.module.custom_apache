00e4c0024460dc9ece9cc4b244b4e37fd18b8861
jsun9406 <jasons@iqmetrix.com>
2021-12-22 12:29:29 -0800

    add

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class customapache {
 }
 
 class customapache::install {
-  if $facts['check_nr_exist'] == "true" {
+  if $facts['check_nr_exist'] == "false" {
     exec { 'Check-Service':
       command   => 'write-host work',
       provider  => powershell,
